aa9e4c41106956119bf0e2f615a302d2fa60f12b
Michael Zaleski <me@mikezaleski.com>
2020-03-27 07:38:05 -0400

    db working and /register endpoint connected

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "michaelzaleski",
+    password: "",
+    database: "smart-brain"
+  }
+});
 
 const app = express();
 
@@ -12,7 +23,7 @@ const database = {
     {
       id: "123",
       name: "John",
-      password: 'cookies',
+      password: "cookies",
       email: "john@gmail.com",
       entries: 0, //used to track scores
       date: new Date() // js method to create date
@@ -20,7 +31,7 @@ const database = {
     {
       id: "124",
       name: "Sally",
-      password: 'bananas',
+      password: "bananas",
       email: "sally@gmail.com",
       entries: 0, //used to track scores
       date: new Date() // js method to create date
@@ -28,10 +39,10 @@ const database = {
   ],
   login: [
     {
-      id: '123',
-      hash: '',
-      email: 'john@gmail.com'
-    },
+      id: "123",
+      hash: "",
+      email: "john@gmail.com"
+    }
   ]
 };
 
@@ -40,12 +51,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-    bcrypt.compare("saladkit", '$2a$10$JDCKtnaWkW9RmP5Cyzuid.Vx7JjKMHXOHxbiQJ7h/VSuCMOJuiZ9m', function(err, res) {
-        console.log('first guess', res)
-    });
-    bcrypt.compare("veggies", '$2a$10$JDCKtnaWkW9RmP5Cyzuid.Vx7JjKMHXOHxbiQJ7h/VSuCMOJuiZ9m', function(err, res) {
-        console.log('second guess', res)
-    });
+  bcrypt.compare(
+    "saladkit",
+    "$2a$10$JDCKtnaWkW9RmP5Cyzuid.Vx7JjKMHXOHxbiQJ7h/VSuCMOJuiZ9m",
+    function(err, res) {
+      console.log("first guess", res);
+    }
+  );
+  bcrypt.compare(
+    "veggies",
+    "$2a$10$JDCKtnaWkW9RmP5Cyzuid.Vx7JjKMHXOHxbiQJ7h/VSuCMOJuiZ9m",
+    function(err, res) {
+      console.log("second guess", res);
+    }
+  );
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -58,15 +77,17 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body; //destructing
-  database.users.push({
-    id: "125", // hardcoded for now
-    name: name,
-    email: email,
-    password: password,
-    entries: 0, //everyone starts with zero
-    date: new Date() // js method to create date
-  });
-  res.json(database.users[database.users.length - 1]); // display last item in array
+  db("users")
+    .returning("*")
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]); // return the first object, since there should onley be one
+    })
+    .catch(err => res.status(400).json('unable to register'));
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -86,21 +107,20 @@ app.get("/profile/:id", (req, res) => {
 });
 
 app.put("/image", (req, res) => {
-    const { id } = req.body; // we rec userId from the body
-    let found = false;
-    database.users.forEach(user => {
-      if (user.id === id) {
-        found = true;
-        user.entries++ // add 1 to the user entries
-        return res.json(user.entries); // display the updated entries
-
-      }
-    });
-    if (!found) {
-      // ie NOT found
-      res.status(404).json("not found");
+  const { id } = req.body; // we rec userId from the body
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++; // add 1 to the user entries
+      return res.json(user.entries); // display the updated entries
     }
   });
+  if (!found) {
+    // ie NOT found
+    res.status(404).json("not found");
+  }
+});
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //     // Store hash in your password DB.
