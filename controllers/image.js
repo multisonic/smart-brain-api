f0832ddf666c7db4c529c5773303c41d7bd7a240
Michael Zaleski <me@mikezaleski.com>
2020-04-01 07:24:37 -0400

    moved Clarifai api calls to server /imageurl

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('Clarifai');
+
+const app = new Clarifai.App({
+  apiKey: "99d13529322f46f5b711574ea9034335"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body; // we rec userId from the body
     db("users")
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
   };
 
   module.exports = {
-      handleImage
+      handleImage,
+      handleApiCall
   };
